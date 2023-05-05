914d3078a1a4e3cd45fb7ad4b39f6083a13ba16d
Hanlynui <hanyu.lin59@gmail.com>
2023-05-05 13:05:23 -0400

    added navbar and searchbar upgrades and part of homepage

--- client/features/searchBar/index.js
+++ client/features/searchBar/index.js
@@ -1,15 +1,31 @@
 import React from "react";
 
-const SearchBar = () => {
+const SearchBar = (props) => {
+  const { name } = props;
   //todo tier2 feature
   return (
     <div>
-      <form>
+      <form className="searchBar">
         <input
           type="search"
           placeholder="Search Name/Ticker"
           aria-label="Search"
         ></input>
+        <span className="notificationBell">
+          <img
+            src="/notificationBell.png"
+            alt="default profile picture"
+            style={{ width: "1rem", height: "1rem" }}
+          ></img>
+        </span>
+        <span className="profilePic&Name">
+          <img
+            src="/defaultPFP.avif"
+            alt="default profile picture"
+            style={{ width: "1rem", height: "1rem" }}
+          ></img>
+          {name}
+        </span>
       </form>
     </div>
   );
