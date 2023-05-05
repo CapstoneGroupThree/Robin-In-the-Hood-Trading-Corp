dc077eb5a64d7b863428f02e9c593ea994584b4c
Hanlynui <hanyu.lin59@gmail.com>
2023-05-05 19:25:40 -0400

    redux should be working i also have a check for if it is a stock trading day so that we dont make aggregate calls on days the market isnt open

--- client/features/home/popularStockView/index.js
+++ client/features/home/popularStockView/index.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
+import { fetchSingleStockName } from "../../singleStock/singleStockSearchSlice";
+import { fetchSingleStockTickerInfo } from "../../singleStock/singleStockSearchSlice";
 
 const PopularStocksHomeView = () => {
   const dispatch = useDispatch();
@@ -73,8 +75,28 @@ const PopularStocksHomeView = () => {
       return selectedTickers;
     }
 
-    const tickers = getRandomTickers();
-    console.log(tickers);
+    // const getName = async () => {
+    //   let tickerInfo = await dispatch(fetchSingleStockName("TSLA"));
+    //   await console.log(tickerInfo.payload.results.name);
+    //   return tickerInfo.payload.results.name;
+    // };
+
+    // getName();
+
+    const getTickerPrice = async () => {
+      let tickerPriceInfo = await dispatch(fetchSingleStockTickerInfo("TSLA"));
+      await console.log(tickerPriceInfo.payload);
+      return tickerPriceInfo.payload;
+    };
+    getTickerPrice();
+
+    // const tickers = getRandomTickers();
+    // console.log(tickers);
+    // const popularStocks = {};
+    // for (let i = 0; i < tickers.length; i++) {
+    //   let ticker = tickers[i];
+    //   popularStocks.ticker = [dispatch(fetchSingleStockName(ticker))];
+    // }
 
     //todo do thunk calls to get the item for the 4 tickers one to tickerdetails one to aggregates per minute
   }, [dispatch]);
--- client/features/singleStock/singleStockSearchSlice/index.js
+++ client/features/singleStock/singleStockSearchSlice/index.js
@@ -6,7 +6,7 @@ export const fetchSingleStockName = createAsyncThunk(
   async (ticker) => {
     try {
       const response = await axios.get(
-        `http://localhost:8080/proxy/ticker-details?ticker=${ticker}`
+        `http://localhost:8080/proxy/rde/ticker-details?ticker=${ticker}`
       );
       return response.data;
     } catch (error) {
@@ -16,9 +16,50 @@ export const fetchSingleStockName = createAsyncThunk(
   }
 );
 
-export const fetchSingleStockTickerPrice = createAsyncThunk(
-  "fetchStockTickerPrice",
-  async (ticker) => {}
+export const fetchSingleStockTickerInfo = createAsyncThunk(
+  "fetchStockTickerInfo",
+  async (ticker) => {
+    const now = new Date();
+    const year = now.getFullYear();
+    const month = String(now.getMonth() + 1).padStart(2, "0");
+    const day = String(now.getDate()).padStart(2, "0");
+
+    const to = `${year}-${month}-${day}`;
+
+    // Adjust the date object to Eastern Time
+    const estOffset = -5 * 60; // Eastern Time is UTC-5
+    const utcOffset = -now.getTimezoneOffset();
+    now.setMinutes(now.getMinutes() + estOffset - utcOffset);
+
+    const dayOfWeek = now.getDay(); // 0 is Sunday, 6 is Saturday
+    const hour = now.getHours();
+    const minute = now.getMinutes();
+
+    // Market is open on weekdays between 9:30 AM and 4:00 PM Eastern Time
+    const marketOpen =
+      dayOfWeek >= 1 &&
+      dayOfWeek <= 5 &&
+      (hour > 9 || (hour === 9 && minute >= 30)) &&
+      hour < 16;
+
+    const from = to;
+    try {
+      if (marketOpen) {
+        const response = await axios.get(
+          `http://localhost:8080/proxy/mde/aggregates?ticker=${ticker}&from=${from}&to=${to}`
+        );
+        return response.data;
+      } else {
+        const response = await axios.get(
+          `http://localhost:8080/proxy/mde/open-close?ticker=${ticker}&date=${to}`
+        );
+        return response.data;
+      }
+    } catch (error) {
+      console.log(error);
+      return error;
+    }
+  }
 );
 
 export const singleStockViewSlice = createSlice({
@@ -27,6 +68,9 @@ export const singleStockViewSlice = createSlice({
   reducers: {},
   extraReducers: (builder) => {
     builder.addCase(fetchSingleStockName.fulfilled, (state, action) => {
+      return action.payload;
+    });
+    builder.addCase(fetchSingleStockTickerInfo.fulfilled, (state, action) => {
       return action.payload;
     });
   },
