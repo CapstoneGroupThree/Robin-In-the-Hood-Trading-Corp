2468cd0cbdb2ef75a757b1e524d3369ac12a493f
Adhemar Hernandez <adhemarhernandez@gmail.com>
2023-05-10 11:17:28 -0400

    smallChange v2

--- client/features/singleStock/singleStockView.js
+++ client/features/singleStock/singleStockView.js
@@ -203,7 +203,11 @@ export default function SingleStockView() {
   }, [dispatch]);
 
   if (isLoading) {
-    return <div> Loading wooooo</div>;
+    return (
+      <div className="flex items-center justify-center min-h-screen">
+        <div className="animate-spin rounded-full h-32 w-32 border-t-4 border-b-4 border-purple-500"></div>
+      </div>
+    );
   }
 
   const handleAddToWatchList = async (e) => {
