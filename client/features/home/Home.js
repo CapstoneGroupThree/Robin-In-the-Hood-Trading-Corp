8ca679c99cc41c6e451af774ddd07b97a7d03204
Adhemar Hernandez <adhemarhernandez@gmail.com>
2023-05-11 12:11:32 -0400

    first commit

--- client/features/home/Home.js
+++ client/features/home/Home.js
@@ -13,12 +13,12 @@ const Home = () => {
   // console.log(displayedName);
   //todo create a search bar feature for pages that need it
   return (
-    <div className="font-semibold flex  flex-col h-screen box-content overflow-hidden">
+    <div className="font-semibold flex flex-col h-screen justify-between max-h-screen overflow-hidden">
       <div className="flex justify-between items-center mb-4">
         <h1>Home</h1>
         <SearchBar name={displayedName} />
       </div>
-      <div className="flex flex-grow h-3/5  overflow-hidden border  ">
+      <div className="flex flex-grow h-2/5 max-h-screen overflow-hidden border  ">
         <div className="w-1/2 h-full flex flex-col box-content">
           <div className="assets h-1/2  border border-gray-400 p-4">
             <h2>Asset</h2>
@@ -29,7 +29,7 @@ const Home = () => {
           </div>
         </div>
 
-        <div className="popularStocks  w-1/2 flex flex-col border border-gray-400 p-4 ">
+        <div className="popularStocks h-2/4 max-h-screen w-1/2 flex flex-col border border-gray-400 p-4 ">
           <h2> Popular Stocks</h2>
 
           <div className="popularStocksHomeView  flex-grow border border-gray-400 p-4">
@@ -37,7 +37,7 @@ const Home = () => {
           </div>
         </div>
       </div>
-      <div className="aibot absolute bottom-0 right-0">
+      <div className="aibot self-end bottom-0 right-0">
         <img
           src="/aiChatRB.png"
           alt="your AI chat assistant "
