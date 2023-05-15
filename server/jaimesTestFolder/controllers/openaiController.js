b85a71fb869f861a8bb6355b62130cd0fd5b6fc2
Hanlynui <hanyu.lin59@gmail.com>
2023-05-15 19:45:02 -0400

    got bare minimum react popup for buy sell feature, now working a little on chatgpt feature BE

--- server/jaimesTestFolder/controllers/openaiController.js
+++ server/jaimesTestFolder/controllers/openaiController.js
@@ -12,42 +12,43 @@ require("dotenv").config();
 // );
 
 // prep for openai api call authorization
-const openai = axios.create({
-  headers: {
-    Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
-    "Content-Type": "application/json",
-  },
-});
+// const openai = axios.create({
+//   headers: {
+//     Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+//     "Content-Type": "application/json",
+//   },
+// });
 
-// Function that takes prompt and returns a response from openai
-async function getOpenaiResponse(req, res, next) {
-  try {
-    const model = "text-davinci-003";
-    const prompt = req.body.prompt;
+// // Function that takes prompt and returns a response from openai
+// async function getOpenaiResponse(req, res, next) {
+//   try {
+//     const model = "gpt-3.5-turbo";
+//     const prompt = req.body.prompt;
 
-    const response = await openai.post(
-      "https://api.openai.com/v1/completions",
-      {
-        model: model,
-        prompt: prompt,
-        max_tokens: 500,
-        temperature: 0.5,
-        top_p: 1,
-        n: 1,
-        frequency_penalty: 0.0,
-        presence_penalty: 0.6,
-      }
-    );
-    console.log("Response from OpenAI API: ", response.data.choices[0].text);
-    // return response.data.choices[0].text.trim();
-    res.status(200).send({
-      bot: response.data.choices[0].text,
-    });
-  } catch (err) {
-    console.log("Error ocurred while calling OpenAI API: ", err.response.data);
-    // return handleError(err);
-    next(err);
-  }
-}
+//     const response = await openai.post(
+//       "https://api.openai.com/v1/chat/completions",
+//       {
+//         model: model,
+//         prompt: prompt,
+//         max_tokens: 500,
+//         temperature: 0.5,
+//         top_p: 1,
+//         n: 1,
+//         frequency_penalty: 0.0,
+//         presence_penalty: 0.6,
+//       }
+//     );
+//     console.log("Response from OpenAI API: ", response.data.choices[0].text);
+//     // return response.data.choices[0].text.trim();
+//     res.status(200).send({
+//       bot: response.data.choices[0].text,
+//     });
+//   } catch (err) {
+//     console.log("Error ocurred while calling OpenAI API: ", err.response.data);
+//     // return handleError(err);
+//     next(err);
+//   }
+// }
 
-module.exports = { getOpenaiResponse };
+// module.exports = { getOpenaiResponse };
+import { Configuration, OpenAIApi } from "openai";
