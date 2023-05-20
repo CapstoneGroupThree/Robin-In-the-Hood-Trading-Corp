a71e4d456aab7140c172e18185bf65a35ae5d04d
Hanlynui <hanyu.lin59@gmail.com>
2023-05-20 10:50:03 -0400

    navbar upgrades

--- client/features/navbar/Navbar.js
+++ client/features/navbar/Navbar.js
@@ -1,18 +1,24 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate, useLocation } from "react-router-dom";
 import { logout } from "../../app/store";
 import AuthForm from "../auth/AuthForm";
 import { resetPortfolio } from "../portfolio/portfolioSlice";
 
 const Navbar = () => {
   const isLoggedIn = useSelector((state) => !!state.auth.me.id);
+  const location = useLocation();
   const dispatch = useDispatch();
   const navigate = useNavigate();
+
   const logoutAndRedirectHome = () => {
     dispatch(logout());
     dispatch(resetPortfolio());
     navigate("/login");
+  };
+
+  const isActiveLink = (pathname) => {
+    return location.pathname === pathname;
   };
 
   return (
@@ -62,44 +68,56 @@ const Navbar = () => {
 
       <nav className="w-full text-xl text-shadow-lg ">
         {isLoggedIn ? (
-          <div className="flex flex-col items-center place-content-evenly ">
+          <div className="flex flex-col items-center">
             {/* The navbar will show these links after you log in */}
-            <Link
-              to="/home"
-              className="hover:text-blue-500 block font-head text-xl"
-            >
-              <i className="fa-solid fa-house-user"></i> {" Home"}
-            </Link>
-            <Link
-              to="/allStocks"
-              className="hover:text-blue-500 block font-head text-xl"
-            >
-              <i className="fa-solid fa-money-bill-1-wave"></i> {" All Stocks"}
-            </Link>
-            <Link
-              to={"/portfolio"}
-              className="hover:text-blue-500 block font-head text-xl"
-            >
-              <i className="fa-solid fa-rocket"></i>
-              {" Portfolio"}
-            </Link>
-            <Link
-              to="/user/edit"
-              className="hover:text-blue-500 block font-head text-xl"
-            >
-              <i className="fa-solid fa-user-pen"></i>
-              {" Edit User Profile"}
-            </Link>
+            <div className="flex flex-col items-start">
+              <Link
+                to="/home"
+                className={`hover:text-blue-500 flex items-center mb-3 ${
+                  isActiveLink("/home") ? "active-link edit-button" : ""
+                }`}
+              >
+                <i className="fa-solid fa-house-user mr-3"></i>
+                <span className="font-head text-xl">Home</span>
+              </Link>
+              <Link
+                to="/allStocks"
+                className={`hover:text-blue-500 flex items-center mb-3 ${
+                  isActiveLink("/allStocks") ? "active-link edit-button" : ""
+                }`}
+              >
+                <i className="fa-solid fa-money-bill-1-wave mr-3"></i>
+                <span className="font-head text-xl">All Stocks</span>
+              </Link>
+              <Link
+                to="/portfolio"
+                className={`hover:text-blue-500 flex items-center mb-3 ${
+                  isActiveLink("/portfolio") ? "active-link edit-button" : ""
+                }`}
+              >
+                <i className="fa-solid fa-rocket mr-3"></i>
+                <span className="font-head text-xl">Portfolio</span>
+              </Link>
+              <Link
+                to="/user/edit"
+                className={`hover:text-blue-500 flex items-center mb-3 ${
+                  isActiveLink("/user/edit") ? "active-link edit-button" : ""
+                }`}
+              >
+                <i className="fa-solid fa-user-pen mr-3"></i>
+                <span className="font-head text-xl">Edit User Profile</span>
+              </Link>
+            </div>
 
-            <hr className=" w-56 fixed bottom-8  border-t-4 border-sky-500 border-opacity-20 pb-4 " />
+            <hr className="w-56 fixed bottom-8 border-t-4 border-sky-500 border-opacity-20 pb-4" />
             <div className="fixed bottom-0 pb-4">
               <button
                 type="button"
-                className="hover:text-blue-500  active:bg-gray-500 active:text-white "
+                className="hover:text-blue-500 active:bg-gray-500 active:text-white"
                 onClick={logoutAndRedirectHome}
               >
-                <i className="fa-solid fa-right-from-bracket"></i>
-                {" Logout"}
+                <i className="fa-solid fa-right-from-bracket mr-3"></i>
+                {"Logout"}
               </button>
             </div>
           </div>
