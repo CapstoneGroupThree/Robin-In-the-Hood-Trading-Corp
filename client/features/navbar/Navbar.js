36f6cfdf817951bebda2980cfe4a6aad13edaeac
Hanlynui <hanyu.lin59@gmail.com>
2023-05-20 11:57:39 -0400

    fixed bug where after logging in or on / routes the navbar effects weren't working properly

--- client/features/navbar/Navbar.js
+++ client/features/navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Link, useNavigate, useLocation } from "react-router-dom";
 import { logout } from "../../app/store";
@@ -16,6 +16,10 @@ const Navbar = () => {
     dispatch(resetPortfolio());
     navigate("/login");
   };
+
+  useEffect(() => {
+    navigate("/home");
+  }, [navigate]);
 
   const isActiveLink = (pathname) => {
     return location.pathname === pathname;
